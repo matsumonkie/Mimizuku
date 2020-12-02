7b0045e090ab9558aa2419be8887410082fa4444
iori <matsuhar@gmail.com>
2020-12-02 10:18:57 +0100

    example: add sum function

--- example/src/Lib.hs
+++ example/src/Lib.hs
@@ -2,12 +2,18 @@ module Lib
     ( someFunc
     ) where
 
+import           Data.Foldable (foldl')
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
 add :: Int -> Int -> Int
 add x y =
   x + y
+
+sum :: [Int] -> Int
+sum xs =
+  foldl' add 0 xs
 
 foo :: String
 foo =
